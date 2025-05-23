86e550042615f60cea01ad2303c237e7ac81289a
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2025-05-23 15:32:32 +0530

    Completed The Dark mode feature

---
+++ app.js
@@ -0,0 +1,24 @@
+let stateMode = 0
+let choice = document.querySelectorAll(".choice")
+
+function switchMode(){
+    if (stateMode == 0) {
+        console.log("Dark Mode on");
+        document.body.style.backgroundColor = "#313E51"
+        document.body.style.color = "#fff"
+        choice.forEach(element => {
+            element.style.backgroundColor = "#fff"
+            element.style.color = "#3B4D66"
+        });
+        stateMode = 1
+    } else{
+        console.log("Lign Mode on");
+        document.body.style.backgroundColor = "#fff"
+        document.body.style.color = "#313E51"
+        choice.forEach(element => {
+            element.style.backgroundColor = "#3B4D66"
+            element.style.color = "#fff"
+        });
+        stateMode = 0
+    }
+}
